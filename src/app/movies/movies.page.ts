0120d90a454d33868c3a9bf4aa6395845fcd0d0e
vahdian <germanpalero@gmail.com>
2021-09-15 00:16:55 +0200

    Functional search API

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -6,23 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
   hello = 'hellow';
-  movies = [
-    { id: '001', title: 'Blade Runner', year: '2021' },
-    { id: '002', title: 'Simbad', year: '2009' },
-    { id: '003', title: 'Matrix', year: '1999' },
-  ];
-  filterMovie = "batman"
-  
-
-  constructor() {
-    
+  movies = [];
+  filterMovie = '';
+  getInput(val) {
+    console.warn(val);
+    this.filterMovie = val;
+    fetch(`http://www.omdbapi.com/?s=${this.filterMovie}&apikey=fbf39e7`)
+      .then((res) => res.json().then((res) => (this.movies = res.Search)))
+      .then((res) => console.log(res));
   }
 
-  ngOnInit() {
-    fetch(`http://www.omdbapi.com/?s=${this.filterMovie}&apikey=fbf39e7`).then((res) =>
-      res.json().then((res) =>
-      this.movies = res.Search
-      )
-    ).then(res=> console.log(res));
-  }
+  constructor() {}
+
+  ngOnInit() {}
 }
